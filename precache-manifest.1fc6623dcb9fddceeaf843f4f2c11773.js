62fc820af75238a1206b24c801c4396801f9ba46
s8sachin <s.sachin94@gmail.com>
2020-06-21 19:16:17 +0530

    keeping build on master branch

---
+++ precache-manifest.1fc6623dcb9fddceeaf843f4f2c11773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0188ead6aee7696a3c7eeaabaf31a4bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba80c2cf8f37778acb1d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1a33c07eca0d20567058",
+    "url": "/static/css/main.a4cfba43.chunk.css"
+  },
+  {
+    "revision": "ba80c2cf8f37778acb1d",
+    "url": "/static/js/2.945464a4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.945464a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a33c07eca0d20567058",
+    "url": "/static/js/main.f17a0740.chunk.js"
+  },
+  {
+    "revision": "9f0e2f2630ca9dcd4f57",
+    "url": "/static/js/runtime-main.8c109fa6.js"
+  }
+]);
