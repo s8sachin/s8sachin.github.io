ba70bd8136a3968138c2cad2b2c3285c5ee58d78
s8sachin <s.sachin94@gmail.com>
2020-06-22 03:13:42 +0530

    handle redirection

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Home from './Modules/Home';
 import FourOhFour from './Modules/FourOhFour';
 import Skills from './Modules/Skills';
 import Projects from './Modules/Projects';
 import Contact from './Modules/Contact';
 
+const RedirectHoc = props => {
+  const { location: { search } } = props;
+  if (search) {
+    const [,redirectPageUrl] = search.split('p=');
+    if (redirectPageUrl) {
+      return <Redirect to={redirectPageUrl} />;
+    }
+  }
+  return <Route exact path="/" component={Home} />;
+};
+
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL} >
       <Switch>
-        <Route exact path="/" component={Home} />
+        <RedirectHoc exact path="/" component={Home} />
         <Route exact path="/skills" component={Skills} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/contact" component={Contact} />
