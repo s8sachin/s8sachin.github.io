0828568ba93c38326edb3b741ca688f8dde74c53
s8sachin <s.sachin94@gmail.com>
2020-07-12 18:39:31 +0530

    projects page

--- src/Modules/Projects.jsx
+++ src/Modules/Projects.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import CustomNav from '../Components/NavBar';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { projectsList } from '../utils/helperList';
 
 const Projects = () => {
   return(
@@ -8,7 +9,25 @@ const Projects = () => {
       <CustomNav />
       <Container className="mt-5">
         {/* <h2 className="color-customBlue">Weekend / hobby projects</h2> */}
-        
+        {projectsList.map(proj => (
+          <Fragment key={proj.label}>
+            <Row className="my-5 px-md-5" xs={1} md={2}>
+              <Col>
+                <img className="img-fluid proj-image shadow" src={proj.imgSrc} alt={proj.label} />
+              </Col>
+              <Col className="pt-2">
+                <h3 className="font-weight-bold color-customBlue mb-0">{proj.label}</h3><br />
+                {proj.description && <>
+                  <span>{proj.description}</span><br />
+                </>}
+                Link: <a href={proj.src} target="_blank" rel="noopener noreferrer">{proj.src}</a><br /><br />
+                {proj.githubSrc && <>
+                  Source code: <a href={proj.githubSrc} target="_blank" rel="noopener noreferrer">{proj.githubSrc}</a><br />
+                </>}
+              </Col>
+            </Row>
+          </Fragment>
+        ))}
       </Container>
     </>
   )
