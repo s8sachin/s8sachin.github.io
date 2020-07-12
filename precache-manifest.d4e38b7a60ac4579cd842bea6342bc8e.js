b5e292c72f811e0d70368c21c934f85c06f85b45
s8sachin <s.sachin94@gmail.com>
2020-07-12 18:40:11 +0530

    Updates

---
+++ precache-manifest.d4e38b7a60ac4579cd842bea6342bc8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb321a0adbe73d8c25b72384bc86448",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4b567423c58a24b7f00",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ae6e07cac4d8ccb2865d",
+    "url": "/static/css/main.0171ded9.chunk.css"
+  },
+  {
+    "revision": "b4b567423c58a24b7f00",
+    "url": "/static/js/2.9a03252b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9a03252b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae6e07cac4d8ccb2865d",
+    "url": "/static/js/main.a3549194.chunk.js"
+  },
+  {
+    "revision": "9f0e2f2630ca9dcd4f57",
+    "url": "/static/js/runtime-main.8c109fa6.js"
+  }
+]);
