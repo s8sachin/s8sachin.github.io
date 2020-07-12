f14a18a2af15839e3bbbdc10ab230817526c0509
s8sachin <s.sachin94@gmail.com>
2020-07-12 14:47:55 +0530

    Skills page changes

--- src/Components/IconCard.jsx
+++ src/Components/IconCard.jsx
@@ -7,7 +7,7 @@ const IconCard = ({
     <div>
       <div 
         className="d-flex justify-content-center align-items-center m-2" 
-        style={{width: 120, height: 120}}
+        style={{width: 90, height: 90}}
         // style={{ background: `url(${src}) no-repeat center center`, width: 150, height: 150 }}
       >
         {/* <center> */}
--- src/Modules/Skills.jsx
+++ src/Modules/Skills.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import CustomNav from '../Components/NavBar';
-import { Container, Row, ProgressBar, FormLabel } from 'react-bootstrap';
+import { Container, Row, ProgressBar, FormLabel, Col } from 'react-bootstrap';
 import IconCard from '../Components/IconCard';
 import { showcaseSkills, skillsList } from '../utils/helperList';
 
@@ -16,17 +16,23 @@ const Skills = () => {
         // , position: 'absolute', top: 0, bottom: 0, left: 0, right: 0
         }}
       >
-        <h2 className="color-customBlue">Tech stack experience</h2>
-        <div className="p-3 shadow">
-          {showcaseSkills.map(skill => <>
-            <FormLabel className="color-customBlue">{skill.label}</FormLabel>
-            <ProgressBar now={skill.rating} variant="info" className="" /><br />
-          </>)}
-        </div>
-        <Row className="my-5 justify-content-evenly">
-          {skillsList.map(s => (
-            <IconCard src={s.src} />
-          ))}
+        {/* <h2 className="color-customBlue">Tech stack experience</h2> */}
+        <Row xs={1} md={2}>
+          <Col>
+            <div className="py-3">
+              {showcaseSkills.map(skill => <>
+                <FormLabel className="color-customBlue">{skill.label}</FormLabel>
+                <ProgressBar now={skill.rating} variant="progress-progCustomBlue" style={{height: 4}} className="" /><br />
+              </>)}
+            </div>
+          </Col>
+          <Col>
+            <Row className="justify-content-evenly">
+              {skillsList.map(s => (
+                <IconCard src={s.src} />
+              ))}
+            </Row>
+          </Col>
         </Row>
       </Container>
     </>
